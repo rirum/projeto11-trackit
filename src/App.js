773968d90c7df13130051350d26b965c93089226
camille <c.miyashita@gmail.com>
2022-12-15 20:20:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import CreateAccount from "./pages/CreateAccount";
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContainerApp>
+      <Login/>
+      {/* <CreateAccount></CreateAccount> */}
+    </ContainerApp>
   );
 }
 
 export default App;
+
+const ContainerApp = styled.div`
+width:100vw;
+min-height: 100vh;
+margin: 0px;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 0px;
+padding: 0px;
+padding-bottom: 200px;
+font-family: 'Lexend Deca', sans-serif;
+`
---
+++ src/pages/CreateAccount.js
@@ -0,0 +1,67 @@
+import logo from "../assets/logo-trackit.png";
+import styled from "styled-components";
+
+export default function CreateAccount() {
+    return(
+        <>
+        <ContainerLogo> 
+            <img src={logo} alt="logo"/> 
+        </ContainerLogo>
+        
+        <LoginInputs placeholder="email"></LoginInputs>
+        <LoginInputs placeholder="senha"></LoginInputs>
+        <LoginInputs placeholder="nome"></LoginInputs>
+        <LoginInputs placeholder="foto"></LoginInputs>
+
+        <StyledButton> <p>Cadastrar</p></StyledButton>
+
+        <SignUpLink><p>Já tem uma conta? Faça login!</p></SignUpLink>
+        
+        </>
+    )
+}
+
+const ContainerLogo = styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+width: 180px;
+height: 180px;
+margin-top: 68px;
+margin-bottom: 36px;
+`
+
+const LoginInputs = styled.input `
+        width: 303px;
+        height: 45px;
+        display: flex;
+        border: 1px solid #d4d4d4;
+        border-radius: 5px;
+        flex-direction: column;
+        padding-left: 11px;
+        margin-bottom: 6px;
+        box-sizing: border-box;
+        font-family: 'Lexend Deca', sans-serif;
+        &::placeholder {
+            font-size: 20px;
+            color: grey; 
+            padding-left: 11px; 
+        }
+`
+const StyledButton = styled.button`
+width: 303px;
+height:45px;
+background-color: #52B6FF;
+border: 5px solid #52B6FF;
+border-radius: 5px;
+font-family: 'Lexend Deca', sans-serif;
+    p{
+        color: white;
+        font-size: 21px;
+    }
+`
+const SignUpLink = styled.div`
+font-size: 14px;
+color: #52B6FF;
+margin-top: 25px;
+`
