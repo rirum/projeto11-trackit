98787638ee70ffaf10ca0a3d4d4e75173a47a2b5
camille <c.miyashita@gmail.com>
2022-12-17 00:59:52 -0300

    import botao disabled

--- src/pages/Habits.js
+++ src/pages/Habits.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import TitleHabits from "../components/TitleHabits";
 import { BsTrash } from 'react-icons/bs';
+import { ThreeDots } from "react-loader-spinner";
 
 export default function Habits(){
     return (
